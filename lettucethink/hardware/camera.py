671386bc6d15e1bbbec3b9352be177258a9405a9
kodda <koddda@gmail.com>
2018-03-21 16:16:12 +0100

    added HD mode for camera

--- lettucethink/hardware/camera.py
+++ lettucethink/hardware/camera.py
@@ -4,26 +4,28 @@ import cv2
 import numpy as np
 
 grab_data={"sync"  : ds.getSyncMap,
-          "uv"    : ds.getUVMap,
-          "conf"  : ds.getConfidenceMap,
-          "vertFP": ds.getVerticesFP,
-          "rgb"   : ds.getColorMap,
-          "vert"  : ds.getVertices,
-          "depth" : ds.getDepthMap}
+           "uv"    : ds.getUVMap,
+           "conf"  : ds.getConfidenceMap,
+           "vertFP": ds.getVerticesFP,
+           "rgb"   : ds.getColorMap,
+           "vert"  : ds.getVertices,
+           "depth" : ds.getDepthMap,
+           "HD"    :  ds.getHDColorMap}
 
 class Camera(object):
     '''
     DepthSense camera-suffix is usually index coded on 3 digits
     ''' 
-    def __init__(self):
-       ds.start()
+    def __init__(self, mode="depth"):
+       if   mode=="depth": ds.start()
+       elif mode=="HD"   : ds.start_HD()
 
     def grab_datas(self, svg, suffix=0,
                    datas=["sync", "uv", "conf", "vertFP",
                           "rgb", "vert","depth"], pics=True):
        for d in datas:
           res=grab_data[d]()
-          if d in ["sync", "rgb"]:
+          if d in ["sync", "rgb","HD"]:
               cv2.imwrite(svg+"%s-%03d.png"%(d,suffix), res.astype(np.uint8))
           elif d in ["conf", "depth", "vert"]:
               cv2.imwrite(svg+"%s-%03d.png"%(d,suffix), res.astype(np.uint16))
