e40e6a390f841b0f725316059b110788bd30cb85
Timothée Wintz <timothee@timwin.fr>
2018-03-27 14:25:34 +0200

    changed archiving

--- lettucethink/utils.py
+++ lettucethink/utils.py
@@ -3,16 +3,15 @@ import zipfile
 import numpy as np
 import os
 
-def createArchive(scandir='scan/', svg="all.zip"):
-    files=os.listdir(scandir)
+def createArchive(files, output_archive="all.zip"):
     zf = zipfile.ZipFile(svg, mode = 'w')
     try:    
         for f in files:
-           print "adding", f["name"]
-           zf.write(scandir+f["name"])
+           print "adding", f
+           zf.write(scandir+f)
     finally:
         zf.close()
-        return {"href": svg, "name": svg}
+        return {"href": output_archive, "name": output_archive}
 
 def clamp(value, lims, scale=1):
     return int(scale*np.clip(value, lims[0], lims[1]))
