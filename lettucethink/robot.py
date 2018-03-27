11203f151c63510cad347c5cb8a49da49d5129eb
Timothée Wintz <timothee@timwin.fr>
2018-03-27 14:25:21 +0200

    support for gphoto

--- lettucethink/robot.py
+++ lettucethink/robot.py
@@ -7,49 +7,129 @@ import numpy as np
 import os
 
 class Robot(object):
-    def __init__(self, cnc_port="/dev/ttyUSB1", gimbal_port="/dev/ttyUSB0", scandir=".", x=0, y=0, z=0, pan=0, tilt=0, homing=True):
-        self.cnc     = grbl_cnc.GrblCNC(cnc_port,x=x, y=y, z=z,homing=homing)
+    @staticmethod
+    def create_robot_grbl_ds_xl430(cnc_port="/dev/ttyUSB0",
+                                   gimbal_port="/dev/ttyUSB1",
+				   homing=True):
+	"""
+	Creates a robot with:
+	- GRBL CNC
+	- DepthSense Camera
+	- XL430 Gimbal
+	:param cnc_port: CNC Port (/dev/ttyUSB0 by default)
+	:param gimbal_port: CNC Port (/dev/ttyUSB1 by default)
+	:param homing: whether to perform homing or not on the CNC
+	"""
+        robot = Robot()
+        self.cnc     = grbl_cnc.GrblCNC(cnc_port, homing=homing)
         self.bracket = XL430_gimbal.XL430(gimbal_port)
         self.cam     = ds_camera.DSCamera()
-        self.scandir = scandir
-        self.t0      = tilt
+	return robot
+
+    @staticmethod
+    def create_robot_grbl_gp2_xl430(cnc_port="/dev/ttyUSB0",
+                                   gimbal_port="/dev/ttyUSB1",
+				   homing=True):
+	"""
+	Creates a robot with:
+	- GRBL CNC
+	- Gphoto2 Camera
+	- XL430 Gimbal
+	:param cnc_port: CNC Port (/dev/ttyUSB0 by default)
+	:param gimbal_port: CNC Port (/dev/ttyUSB1 by default)
+	:param homing: whether to perform homing or not on the CNC
+	"""
+        robot = Robot()
+        self.cnc     = grbl_cnc.GrblCNC(cnc_port, homing=homing)
+        self.bracket = XL430_gimbal.XL430(gimbal_port)
+        self.cam     = gp2_camera.GP2Camera()
+	return robot
+
+    def __init__(self):
+	self.cnc = None
+	self.bracket = None
+	self.cam = None
+	self.has_started = False
 
     def start(self):
+        """
+        Starts the scanner, by starting cnc, bracket and camera.
+        """
+	if cnc is None:
+            raise ValueError("CNC is not defined")
+        if bracket is None:
+            raise ValueError("Bracket is not defined")
+        if cam is None:
+            raise ValueError("Camera is not defined")
         self.cnc.start()
         self.bracket.start()        
         self.cam.start()
+        self.has_started = True
         
-    def scan_at(self, x, y, z, pan, tilt, i, dt=2):
+    def scan_at(self, x, y, z, pan, tilt, suffix="", wait_time=2):
+        """
+        Moves arm to position (x,y,z,pan,tilt) and acquire data from camera.
+        :param x: position x
+        :param y: position y
+        :param z: position z
+        :param pan: orientation pan
+        :param tilt: orientation tilt
+        :param suffix: will be added to the file name
+        :param wait_time: time to wait after movement before taking the shot
+        """
+        if not self.has_started:
+            raise RuntimeError("You must call self.start() before scanning")
         self.bracket.move_to(pan, tilt)
         self.cnc.move_to(x, y, z)
-        time.sleep(dt)
-        self.cam.grab_write_all(self.scandir, str(i).zfill(3))
-        return
+        time.sleep(wait_time)
+        return self.cam.grab_write_all(self.scandir, suffix)
 
-    def circular_scan(self, xc, yc, zc, r, nc, svg="all.zip"):
+    def circular_scan(self, xc, yc, zc, r, nc, output_archive="all.zip"):
+        """
+        Scans along a circular path
+        :param xc: center x
+        :param yc: center y
+        :param zc: center z
+        :param r: radius
+        :param nc: number of points
+        :param output_archive: where to save zip file scan
+        """
         self.bracket.start()
         self.files = []
-        traj=[]
+        scan_file = open(self.scandir + "/scan.csv", “w”) 
+        header_string = "x\ty\tz\tpan\ttilt\t"
+        for data in cam.available_data:
+            header_string = header_string + data + "\t")
+        header_string = header_string[:-1] #Remove last tab
+        scan_file.write(header_string)
         x, y, pan = scanpath.circle(xc, yc, r, nc)
-        x=x[::-1]
-        y=y[::-1]
-        pan=pan
         
         for i in range(0, nc):
            xi, yi, zi = self.xyz_clamp(x[i], y[i], zc)
-           #pi, ti  = self.pantilt_clamp(pan[i], self.t0)
-           pi,ti=pan[i], self.t0
-           #print(pi)
-           traj.append([xi,yi,zi,pi,ti])
-           self.scan_at(xi, yi, zi, pi, ti, i)
+           pi, ti = pan[i], self.t0
+           res = self.scan_at(xi, yi, zi, pi, ti, str(i).zfill(3))
+           d = self.get_position()
+           line_string = str(d['x']) + "\t" + str(d['y']) + "\t" + str(d['z']) + '\t' + str(d['pan']) + '\t' + str(d['tilt'])
+           for x in res:
+               line_string + x + '\t'
+           line_string = line_string[:-1]
+           scan_file.write(line_string)
+           self.files.extend(res)
+
+        self.files.extend(self.scandir + "/scan.csv")
+
        
         self.cnc.move_to(x[0], y[0], zc)
         self.bracket.move_to(0, self.t0)
-        np.save(self.scandir+"traj", np.asarray(traj))
-        ut.createArchive(self.scandir, svg)
+
+        ut.createArchive(self.files, output_archive)
  
     def get_position(self):
-        return {'x': self.cnc.x, 'y': self.cnc.y, 'z': self.cnc.z, 'pan': self.bracket.pan, 'tilt': self.bracket.tilt }
+        return {'x': self.cnc.x,
+                'y': self.cnc.y,
+                'z': self.cnc.z,
+                'pan': self.bracket.pan,
+                'tilt': self.bracket.tilt}
 
     def xyz_clamp(self, x, y, z):
         return ut.clamp(x, self.cnc.x_lims), ut.clamp(y, self.cnc.y_lims), ut.clamp(z, self.cnc.z_lims)
