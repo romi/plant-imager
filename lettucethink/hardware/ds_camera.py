1b876ffda4d0dbfc399e1f366d63a25f21a28074
Timothée Wintz <timothee@timwin.fr>
2018-03-27 10:18:19 +0200

    removed enum types

--- lettucethink/hardware/ds_camera.py
+++ lettucethink/hardware/ds_camera.py
@@ -3,20 +3,12 @@ import pydepthsense as ds
 import tifffile
 from enum import Enum
 
-class DSCameraData(Enum):
-    sync = "sync"
-    uv = "uv"
-    confidence = "confidence"
-    float_vertices = "vertices_fp"
-    vertices = "vertices"
-    color = "rgb"
-    depth = "depth"
-    color_hd = "rgb_hd"
-
 
 class DSCameraMode(Enum):
     depth = 1
     hd = 2
+
+
 
 def singleton(class_):
     instances = {}
@@ -26,39 +18,40 @@ def singleton(class_):
         return instances[class_]
     return instance
 
+modes = {"depth", "hd"}
 @singleton
 class DSCamera():
     '''
     DepthSense Camera object.
     ''' 
 
-    def __init__(self, mode=DSCameraMode.depth):
+    def __init__(self, mode="depth"):
         """
         Constructor. Must call start() before use
         """
-        if not isinstance(mode, DSCameraMode):
-            raise TypeError(data)
+        if mode not in modes:
+            raise ValueError("Incorrect mode")
         self.mode = mode
-        if (self.mode == DSCameraMode.depth):
-            self.available_data = {DSCameraData.sync,
-                    DSCameraData.uv,
-                    DSCameraData.confidence,
-                    DSCameraData.float_vertices,
-                    DSCameraData.vertices,
-                    DSCameraData.color,
-                    DSCameraData.depth }
-            self.default_data = DSCameraData.depth
-        elif (self.mode == DSCameraMode.depth):
-            self.available_data = {DSCameraData.color_hd}
-            self.default_data = DSCameraData.color_hd
+        if (self.mode == "depth"):
+            self.available_data = {"sync"
+                    "uv",
+                    "confidence",
+                    "vertices_fp",
+                    "vertices",
+                    "rgb",
+                    "depth" }
+            self.default_data = "depth"
+        elif (self.mode == "hd"):
+            self.available_data = {"rgb_hd"}
+            self.default_data = "rgb_hd"
         self.is_started = False
 
     def start(self):
         if self.is_started:
             return
-        if (self.mode == DSCameraMode.depth):
+        if (self.mode == "depth"):
             ds.start()
-        elif (self.mode == DSCameraMode.depth):
+        elif (self.mode == "hd"):
             ds.start_HD()
         self.is_started = True
 
@@ -71,7 +64,7 @@ class DSCamera():
     def grab_write_all(self, suffix, data=None):
         """
         Grabs all data in data and writes it to x${suffix}.tif
-        where x in the data name (see DSCameraData)
+        where x is the data name (in available_data) 
         """
         res = []
         if data is None:
@@ -99,24 +92,22 @@ class DSCamera():
         assert(self.is_started)
         if data is None:
             data = self.default_data
-        if not isinstance(data, DSCameraData):
-            raise TypeError(data)
         if data not in self.available_data:
-            raise ValueError(data)
+            raise ValueError("Incorrect data type")
 
-        if data == DSCameraData.sync:
+        if data == "sync":
             return ds.getSyncMap()
-        elif data == DSCameraData.uv:
+        elif data == "uv":
             return ds.getUVMap()
-        elif data == DSCameraData.confidence:
+        elif data == "confidence":
             return ds.getConfidenceMap()
-        elif data == DSCameraData.float_vertices:
+        elif data == "vertices_fp":
             return ds.getVerticesFP()
-        elif data == DSCameraData.vertices:
+        elif data == "vertices":
             return ds.getVertices()
-        elif data == DSCameraData.color:
+        elif data == "rgb":
             return ds.getColorMap()
-        elif data == DSCameraData.depth:
+        elif data == "depth":
             return ds.getDepthMap()
-        elif data == DSCameraData.color_hd:
+        elif data == "rgb_hd":
             return ds.getHDColorMap()
