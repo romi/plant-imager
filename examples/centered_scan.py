e90d5901fe0ea39a1050a45fe235fcce9a42aa51
kodda <koddda@gmail.com>
2018-04-04 10:37:00 +0200

    added z and tilt options for centered scan

--- examples/centered_scan.py
+++ examples/centered_scan.py
@@ -11,7 +11,6 @@ import time
 import json
 import pydepthsense as pds
 
-
 scan_dir = "scan"
 
 cnc_port = "/dev/ttyUSB0"
@@ -30,7 +29,7 @@ if __name__ == '__main__':
     for opt, arg in opts:
         print(opt)
         if opt == '-h':
-            print('centered_scan.py [-H<homing>] -c <cnc_port=/dev/ttyUSB0> -g <gimbal_port=/dev/ttyUSB1> -d <directory=./scan/> -n <num_points=10> -r <radius=35>')
+            print('centered_scan.py [-H<homing>] -c <cnc_port=/dev/ttyUSB0> -g <gimbal_port=/dev/ttyUSB1> -d <directory=./scan/> -n <num_points=10> -r <radius=35> -z <cnc_z=0 > -t <gimbal_tilt=0>')
             sys.exit()
         if opt == '-H':
             homing = True
@@ -44,7 +43,11 @@ if __name__ == '__main__':
             pars["nc"] = int(arg)
         elif opt == "-r":
             pars["r"] = int(arg)
-
+        elif opt == "-z":
+            pars["z"] = int(arg)
+        elif opt == "-":
+            pars["t"] = int(arg)
+            
     t0=time.time()
 
     json.dump(pars,open(scan_dir+"pars.json","w"))
@@ -52,6 +55,7 @@ if __name__ == '__main__':
     lscan=Robot.create_robot_grbl_ds_xl430(scan_dir=scan_dir,homing=homing,cnc_port=cnc_port,gimbal_port=gimbal_port)
     try:
         lscan.start()
+        lscan.t0=pars["t"]
         lscan.circular_scan(pars["xc"],pars["yc"],pars["zc"],pars["r"],pars["nc"])
 
         t=time.time()-t0
