a234675262ea2b4301918fb8f3061131c69fa2fe
jlegrand62 <jonathan.legrand@ens-lyon.fr>
2018-12-10 18:32:38 +0100

    FileSystem database update
    Add file type
    Add file encoding
    Add file header
    Add short module description
    Add class docstring with attributes description

--- lettucethink/fsdb.py
+++ lettucethink/fsdb.py
@@ -1,79 +1,151 @@
-import os
-import sys
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+#       File author(s):
+#           Peter Hanappe <peter@hanappe.com>
+#
+#       File contributor(s):
+#           Peter Hanappe <peter@hanappe.com>
+#           Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
+#
+#       File maintainer(s):
+#           Peter Hanappe <peter@hanappe.com>
+#
+#       Distributed under XXXXX license.
+#
+# ------------------------------------------------------------------------------
+
+"""Implementation of a database as a local file structure.
+
+Assuming the following file structure::
+
+2018/
+2018/images/
+2018/images/rgb0001.jpg
+2018/images/rgb0002.jpg
+
+The 2018/files.json file then contains the following structure::
+
+{
+    "filesets": [
+        {
+            "id": "images",
+            "files": [
+                {
+                    "id": "rgb00001",
+                    "file": "rgb00001.jpg"
+                },
+                {
+                    "id": "rgb00002",
+                    "file": "rgb00002.jpg"
+                }
+            ]
+        }
+    ]
+}
+
+The metadata of the scan, filesets, and images are stored all as
+json objects in a separate directory::
+
+2018/metadata/
+2018/metadata/metadata.json
+2018/metadata/images.json
+2018/metadata/images/rgb0001.json
+2018/metadata/images/rgb0002.json
+"""
+
+import copy
 import json
-import copy 
-import imageio 
+import os
 from shutil import copyfile
+
+import imageio
+
 from lettucethink import error, db
 
-#
-# Assuming the following file structure:
-#
-# 2018/
-# 2018/images/
-# 2018/images/rgb0001.jpg
-# 2018/images/rgb0002.jpg
-#
-# The 2018/files.json file then contains the following structure:
-#
-# {
-#     "filesets": [
-#         {
-#             "id": "images",
-#             "files": [
-#                 {
-#                     "id": "rgb00001",
-#                     "file": "rgb00001.jpg"
-#                 },
-#                 {
-#                     "id": "rgb00002",
-#                     "file": "rgb00002.jpg"
-#                 }
-#             ]
-#         }
-#     ]
-# }
-#
-# The metadata of the scan, filesets, and images are stored all as
-# json objects in a separate directory:
-# 
-# 2018/metadata/
-# 2018/metadata/metadata.json
-# 2018/metadata/images.json
-# 2018/metadata/images/rgb0001.json
-# 2018/metadata/images/rgb0002.json
-#
 
-            
 class DB(db.DB):
-    
+    """Class defining the database object `DB`.
+
+    Implementation of a database as a simple file structure with:
+      * `images` folder containing image files
+      * `metadata` folder containing JSON metadata associated to image files
+
+    Attributes
+    ----------
+    basedir : str
+        path to the base directory containing the database
+    scans : list
+        list of `Scan` objects found in the database
+
+    Methods
+    -------
+    connect:
+        no connection required here
+    disconnect:
+        no connection required here
+    get_scans:
+        get the list of scans saved in the database
+    get_scan:
+        get a scan save in the database
+    create_scan:
+        create a new scan object in the database
+    """
+
     def __init__(self, basedir):
         if not os.path.isdir(basedir):
             raise error.Error("Not a directory: %s" % basedir)
         self.basedir = basedir
         self.scans = _load_scans(self)
-        
-        
+
     def connect(self, login_data=None):
         pass
 
-    
     def disconnect(self):
         pass
 
-    
     def get_scans(self):
+        """Get the list of scans saved in the database.
+
+        Returns
+        -------
+        scans : list
+            list of `Scan` objects found in the database
+        """
         return self.scans
 
-
     def get_scan(self, id):
+        """Get a scan saved in the database under given `id`.
+
+        Parameters
+        ----------
+        id : int
+            identifier of the scan to get
+
+
+        Returns
+        -------
+        scan : Scan
+            `Scan` object associated to given `id`
+        """
         for scan in self.scans:
             if scan.get_id() == id:
                 return scan
         return None
 
-
     def create_scan(self, id):
+        """Create a new scan object in the database.
+
+        Parameters
+        ----------
+        id : int
+            identifier of the scan to create
+
+        Returns
+        -------
+        scan: Scan
+            a new scan object from the database
+        """
         if not _is_valid_id(id):
             raise error.Error("Invalid id")
         if self.get_scan(id) != None:
@@ -82,37 +154,48 @@ class DB(db.DB):
         _make_scan(scan)
         self.scans.append(scan)
         return scan
-    
-            
+
+
 class Scan(db.Scan):
-    
+    """Class defining the scan object `Scan`.
+
+    Implementation of a scan as a list of files with attached metadata.
+
+    Attributes
+    ----------
+    db : DB
+        database where to find the scan
+    id : int
+        id of the scan in the database `DB`
+    metadata : dict
+        dictionary of metadata attached to `Scan` object
+    filesets : list
+        list of `Fileset` attached to `Scan` object
+    """
+
     def __init__(self, db, id):
-        super().__init__(db, id) 
+        super().__init__(db, id)
         self.metadata = None
         self.filesets = []
-        
-    def get_filesets(self):
-        return self.filesets # Copy?
 
-    
+    def get_filesets(self):
+        return self.filesets  # Copy?
+
     def get_fileset(self, id):
         for fileset in self.filesets:
             if fileset.get_id() == id:
                 return fileset
         return None
 
-
     def get_metadata(self, key=None):
         return _get_metadata(self.metadata, key)
 
-        
     def set_metadata(self, data, value=None):
         if self.metadata == None:
             self.metadata = {}
         _set_metadata(self.metadata, data, value)
         _store_scan_metadata(self)
 
-        
     def create_fileset(self, id):
         if not _is_valid_id(id):
             raise error.Error("Invalid id")
@@ -124,32 +207,28 @@ class Scan(db.Scan):
         self.store()
         return fileset
 
-    
     def store(self):
         _store_scan(self)
 
-        
+
 class Fileset(db.Fileset):
-    
+
     def __init__(self, db, scan, id):
-        super().__init__(db, scan, id) 
+        super().__init__(db, scan, id)
         self.metadata = None
         self.files = []
 
     def get_files(self):
         return self.files
 
-
     def get_metadata(self, key):
         return _get_metadata(self.metadata, key)
 
-        
-    def set_metadata(self, data, value= None):
+    def set_metadata(self, data, value=None):
         if self.metadata == None:
             self.metadata = {}
         _set_metadata(self.metadata, data, value)
         _store_fileset_metadata(self)
-
 
     def create_file(self, id):
         file = File(self.db, self, id, None)
@@ -157,30 +236,25 @@ class Fileset(db.Fileset):
         self.store()
         return file
 
-    
     def store(self):
         self.scan.store()
 
-    
-    
+
 class File(db.File):
-    
+
     def __init__(self, db, fileset, id, filename):
-        super().__init__(db, fileset, id) 
+        super().__init__(db, fileset, id)
         self.filename = filename
         self.metadata = None
 
-        
     def get_metadata(self, key):
         return _get_metadata(self.metadata, key)
 
-        
-    def set_metadata(self, data, value= None):
+    def set_metadata(self, data, value=None):
         if self.metadata == None:
             self.metadata = {}
         _set_metadata(self.metadata, data, value)
         _store_file_metadata(self)
-
 
     def write_image(self, type, image):
         filename = _get_filename(self, type)
@@ -188,8 +262,7 @@ class File(db.File):
         imageio.imwrite(path, image)
         self.filename = filename
         self.store()
-        
-    
+
     def write_text(self, type, string):
         filename = _get_filename(self, type)
         path = _file_path(self, filename)
@@ -198,7 +271,6 @@ class File(db.File):
         self.filename = filename
         self.store()
 
-    
     def write_bytes(self, type, buffer):
         filename = _get_filename(self, type)
         path = _file_path(self, filename)
@@ -207,7 +279,6 @@ class File(db.File):
         self.filename = filename
         self.store()
 
-    
     def import_file(self, path):
         filename = os.path.basename(path)
         newpath = _file_path(self, filename)
@@ -215,28 +286,24 @@ class File(db.File):
         self.filename = filename
         self.store()
 
-    
     def read_image(self):
         path = _file_path(self, self.filename)
         return imageio.imread(path)
 
-    
     def read_text(self):
         path = _file_path(self, self.filename)
         with open(path, "r") as f:
             return f.read()
 
-    
     def read_bytes(self):
         path = _file_path(self, self.filename)
         with open(path, "rb") as f:
             return f.read()
 
-        
     def store(self):
         self.fileset.store()
 
-        
+
 ##################################################################
 #
 # the ugly stuff...
@@ -250,7 +317,7 @@ def _load_scans(db):
     for name in names:
         scan = Scan(db, name)
         if (os.path.isdir(_scan_path(scan))
-            and os.path.isfile(_scan_files_json(scan))):
+                and os.path.isfile(_scan_files_json(scan))):
             scan.filesets = _load_scan_filesets(scan)
             scan.metadata = _load_scan_metadata(scan)
             scans.append(scan)
@@ -277,7 +344,7 @@ def _load_fileset(scan, fileset_info):
     fileset.files = _load_fileset_files(fileset, fileset_info)
     fileset.metadata = _load_fileset_metadata(fileset)
     return fileset
-    
+
 
 def _parse_fileset(db, scan, fileset_info):
     id = fileset_info.get("id")
@@ -286,10 +353,11 @@ def _parse_fileset(db, scan, fileset_info):
     fileset = Fileset(db, scan, id)
     path = _fileset_path(fileset)
     if not os.path.isdir(path):
-        raise error.Error("Fileset: Fileset directory doesn't exists: %s" % path)
+        raise error.Error(
+            "Fileset: Fileset directory doesn't exists: %s" % path)
     return fileset
-    
-    
+
+
 def _load_fileset_files(fileset, fileset_info):
     files = []
     files_info = fileset_info.get("files", [])
@@ -301,7 +369,7 @@ def _load_fileset_files(fileset, fileset_info):
         raise error.Error("files.json: expected a list for files")
     return files
 
-        
+
 def _load_file(fileset, file_info):
     file = _parse_file(fileset, file_info)
     file.metadata = _load_file_metadata(file)
@@ -334,7 +402,7 @@ def _load_metadata(path):
     else:
         return {}
 
-    
+
 def _load_scan_metadata(scan):
     return _load_metadata(_scan_metadata_path(scan))
 
@@ -345,37 +413,38 @@ def _load_fileset_metadata(fileset):
 
 def _load_file_metadata(file):
     return _load_metadata(_file_metadata_path(file))
-        
-                            
+
+
 def _mkdir_metadata(path):
     dir = os.path.dirname(path)
     if not os.path.isdir(dir):
         os.makedirs(dir)
 
-        
+
 def _store_metadata(path, metadata):
     _mkdir_metadata(path)
     with open(path, "w") as f:
         json.dump(metadata, f, sort_keys=True,
                   indent=4, separators=(',', ': '))
 
-        
+
 def _store_scan_metadata(scan):
     _store_metadata(_scan_metadata_path(scan),
                     scan.metadata)
 
-        
+
 def _store_fileset_metadata(fileset):
     _store_metadata(_fileset_metadata_path(fileset),
                     fileset.metadata)
 
-        
+
 def _store_file_metadata(file):
     _store_metadata(_file_metadata_path(file),
                     file.metadata)
 
-# 
-        
+
+#
+
 def _get_metadata(metadata, key):
     # Do a deepcopy of the return value because we don't want to
     # caller the inadvertedly change the values.
@@ -386,7 +455,7 @@ def _get_metadata(metadata, key):
     else:
         return copy.deepcopy(metadata.get(str(key)))
 
-        
+
 def _set_metadata(metadata, data, value):
     if isinstance(data, str):
         if value == None:
@@ -451,14 +520,14 @@ def _scan_metadata_path(scan):
                         "metadata",
                         "metadata.json")
 
-        
+
 def _fileset_metadata_path(fileset):
     return os.path.join(fileset.db.basedir,
                         fileset.scan.id,
                         "metadata",
                         fileset.id + ".json")
 
-        
+
 def _file_metadata_path(file):
     return os.path.join(file.db.basedir,
                         file.fileset.scan.id,
@@ -466,34 +535,36 @@ def _file_metadata_path(file):
                         file.fileset.id,
                         file.id + ".json")
 
+
 # store a scan to disk
 
 def _file_to_dict(file):
-    return { "id": file.get_id(), "file": file.filename }
+    return {"id": file.get_id(), "file": file.filename}
 
 
 def _fileset_to_dict(fileset):
     files = []
     for f in fileset.get_files():
         files.append(_file_to_dict(f))
-    return { "id": fileset.get_id(), "files": files }
+    return {"id": fileset.get_id(), "files": files}
 
-    
+
 def _scan_to_dict(scan):
     filesets = []
     for fileset in scan.get_filesets():
         filesets.append(_fileset_to_dict(fileset))
-    return { "filesets": filesets }
-    
-    
+    return {"filesets": filesets}
+
+
 def _store_scan(scan):
     structure = _scan_to_dict(scan)
     files_json = _scan_files_json(scan)
     with open(files_json, "w") as f:
         json.dump(structure, f, sort_keys=True,
                   indent=4, separators=(',', ': '))
-    
+
+
 #
 
 def _is_valid_id(id):
-    return True # haha  (FIXME!)
+    return True  # haha  (FIXME!)
