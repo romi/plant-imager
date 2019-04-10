aa41c4f821d992885ac427a2f7b2b63f7ac5f57f
jlegrand62 <jonathan.legrand@ens-lyon.fr>
2019-04-10 18:04:39 +0200

    Add conda build recipe and environment dependencies Modified `setup.py` to work with conda build recipe

--- lettucethink/__init__.py
+++ lettucethink/__init__.py
@@ -1,1 +1,2 @@
 __all__ = ["bar", "spam", "eggs"]
+__version__ = 'dev'
--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
+
+import lettucethink
 
 ver_file = os.path.join('lettucethink', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
 
-opts = dict(name=NAME,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=find_packages(),
+# Hack to avoid annoying conda error message
+# Package name are `opencv-python` with PIP & `py-opencv` with CONDA
+pyopencv = "opencv-python"
+conda_build = os.environ.get('CONDA_BUILD', '0') == '1'
+if conda_build:
+    pyopencv = ""
 
+opts = dict(name='lettucethink-python',
+            description="Python tools for the LettuceThink robot",
+            long_description="",
+            license="",
+            platforms=["Linux"],
+            version=lettucethink.__version__,
+            packages=find_packages(),
             install_requires=[
                 "pyserial",
-                "opencv-python"
-                ],
+                pyopencv
+            ],
+            classifiers=[
+                "Programming Language :: Python",
+                "Intended Audience :: Science/Research",
+                "Natural Language :: English",
+                "Programming Language :: Python :: 3.7",
+                "Topic :: Scientific/Engineering",
+            ],
             )
 
 if __name__ == '__main__':
