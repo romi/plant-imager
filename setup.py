57af4f96bb82d3b37f83aae810a22374405769ff
Timothée Wintz <timothee@timwin.fr>
2019-11-29 16:59:33 +0100

    remove deps from setup

--- setup.py
+++ setup.py
@@ -5,13 +5,6 @@ ver_file = os.path.join('lettucethink', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
 
-# Hack to avoid annoying conda error message
-# Package name are `opencv-python` with PIP & `py-opencv` with CONDA
-pyopencv = "opencv-python"
-conda_build = os.environ.get('CONDA_BUILD', '0') == '1'
-if conda_build:
-    pyopencv = ""
-
 opts = dict(name=NAME,
             description=DESCRIPTION,
             long_description=LONG_DESCRIPTION,
@@ -20,10 +13,6 @@ opts = dict(name=NAME,
             platforms=PLATFORMS,
             version=VERSION,
             packages=find_packages(),
-            install_requires=[
-                "pyserial",
-                pyopencv
-            ],
             )
 
 if __name__ == '__main__':
