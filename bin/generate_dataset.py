f78b55fad48b12cef1d9a7c2a035faf97bffb913
Timothée Wintz <timothee@timwin.fr>
2020-02-25 18:12:49 +0100

    tweaks in dataset

--- bin/generate_dataset.py
+++ bin/generate_dataset.py
@@ -21,25 +21,27 @@ db = sys.argv[-2]
 def run(config, scan_name):
     with tempfile.TemporaryDirectory() as tempdir:
         toml.dump(config, open(os.path.join(tempdir, "config.toml"), "w"))
-        subprocess.run(["romi_run_task", "--config", os.path.join(tempdir, "config.toml"), "VirtualScan", os.path.join(db, scan_name), "--local-scheduler", "--log-level", "DEBUG"], check=True)
+        subprocess.run(["romi_run_task", "--config", os.path.join(tempdir, "config.toml"), "VirtualScan", os.path.join(db, scan_name), "--local-scheduler", "--log-level", "WARNING"], check=True)
 
 def basic_scan_config(config):
     config = copy.deepcopy(config)
     config["ScanPath"]["kwargs"]["center_x"] = random.randint(-5, 5)
     config["ScanPath"]["kwargs"]["center_y"] = random.randint(-5, 5)
 
-    angle = random.randint(0, 60)
-    distance = random.randint(20, 45)
-    radius = float(distance * np.cos(angle / 180 * np.pi))
+    angle = random.randint(0, 30)
+    distance = 30
+    radius = distance
 
     config["ScanPath"]["kwargs"]["tilt"] = angle
     config["ScanPath"]["kwargs"]["radius"] = radius
-    config["ScanPath"]["kwargs"]["z"] = float(distance * np.sin(angle / 180 * np.pi)) + random.randint(20, 50)
+    config["ScanPath"]["kwargs"]["z"] = float(distance * np.sin(angle / 180 * np.pi)) + random.randint(25, 40)
 
-    focal = random.randint(20, 45)
-    h = 30
-    focal = 35 / h * radius
+    focal = np.random.randint(20, 35)
     config["VirtualScan"]["scanner"]["focal"] = focal
+    config["VirtualPlant"]["lpy_globals"]["BETA"] = random.randint(50, 90)
+    config["VirtualPlant"]["lpy_globals"]["INTERNODE_LENGTH"] = 0.1 * random.randint(9, 20)
+    config["VirtualPlant"]["lpy_globals"]["STEM_DIAMETER"] = 0.01 * random.randint(9, 20)
+    config["VirtualPlant"]["lpy_globals"]["BETA"] = random.randint(50, 90)
     return config
 
 def no_scene(config):
@@ -62,10 +64,15 @@ def branch_on(config):
     config["VirtualPlant"]["lpy_globals"]["BRANCHON"] = True
     return config
 
-def arabidopsis_small(config):
+def arabidopsis_big(config):
     config = copy.deepcopy(config)
-    config["VirtualPlant"]["lpy_globals"]["MEAN_NB_DAYS"] = 30
+    config["VirtualPlant"]["lpy_globals"]["MEAN_NB_DAYS"] = 50
     config["VirtualPlant"]["lpy_globals"]["STDEV_NB_DAYS"] = 5
+
+    distance = 35
+    radius = distance
+    config["ScanPath"]["kwargs"]["radius"] = radius
+
     return config
 
 orig_config = toml.load(param)
@@ -73,19 +80,17 @@ k = 30
 
 config = basic_scan_config(orig_config)
 config_no_scene = no_scene(config)
-config_no_leaves = no_leaves(config)
-config_no_leaves_no_scene = no_leaves(config_no_scene)
+config_no_leaves = no_leaves(config_no_scene)
 config_branch_on = branch_on(config_no_scene)
-config_small_branch_on = arabidopsis_small(config_branch_on)
-config_small = arabidopsis_small(config_no_scene)
+config_big_branch_on = arabidopsis_big(config_branch_on)
+config_big = arabidopsis_big(config_no_scene)
 
 configs = [config,
 config_no_leaves,
 config_no_scene,
-config_no_leaves_no_scene,
 config_branch_on,
-config_small_branch_on,
-config_small]
+config_big_branch_on,
+config_big]
 
 for i in range(k):
     for j,c in enumerate(configs):
