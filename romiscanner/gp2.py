c1ef3d0029eef05c6366942c48b370e13d64fc52
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2020-08-20 21:06:33 +0200

    Fix gp2 Camera class to grab & write images with gphoto2

--- romiscanner/gp2.py
+++ romiscanner/gp2.py
@@ -27,6 +27,8 @@ import os
 import imageio
 import tempfile
 import atexit
+from io import BytesIO
+from PIL import Image
 
 from romiscanner import hal, error
 from .hal import DataItem
@@ -34,6 +36,21 @@ from .hal import DataItem
 class Camera(hal.AbstractCamera):
     """
     Gphoto2 Camera object.
+
+    Requires system library `libgphoto2-dev` & python package `gphoto2`.
+    ```
+    sudo apt-get install libgphoto2-dev
+    pip install gphoto2
+    ```
+
+    Examples
+    --------
+    >>> from romiscanner.gp2 import Camera
+    >>> cam = Camera()
+    >>> img = cam.grab(0)
+    >>> img.show()
+    >>> img_file = cam.grab_write('gp2_img.jpg')
+
     """
 
     def __init__(self):
@@ -57,17 +74,25 @@ class Camera(hal.AbstractCamera):
         return ['rgb']
 
     def grab(self, idx: int, metadata: dict=None):
-        with tempfile.TemporaryDirectory as tmp:
-            fname = os.path.join(tmp, "frame.jpg")
-            self.grab_write(fname)
-            data_item = DataItem(idx, metadata)
-            data = imageio.imread(fname)
-            data_item.add_channel("rgb", data)
-            return data_item
+        # with tempfile.TemporaryDirectory as tmp:
+        #     fname = os.path.join(tmp, "frame.jpg")
+        #     self.grab_write(fname)
+        #     data_item = DataItem(idx, metadata)
+        #     data = imageio.imread(fname)
+        #     data_item.add_channel("rgb", data)
+        #     return data_item
+
+        file_path = self.camera.capture(0)
+        camera_file = self.camera.file_get(file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
+        # Read data using 'get_data_and_size' which allocates its own buffer:
+        file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
+        # Open with PIL:
+        image = Image.open(BytesIO(file_data))
+        return image
 
     def grab_write(self, target: str):
         file_path = self.camera.capture(0)
         camera_file = self.camera.file_get(file_path.folder, file_path.name,
                                            gp.GP_FILE_TYPE_NORMAL)
-        gp.check_result(gp.gp_file_save(camera_file, target))
+        gp.gp_file_save(camera_file, target)
         return target
