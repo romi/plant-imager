3c3bd31638f7c5e48c814e4fd6b5fa8f68102886
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2020-09-10 19:33:00 +0200

    Fix urlcam.py

--- romiscanner/urlcam.py
+++ romiscanner/urlcam.py
@@ -61,7 +61,7 @@ class Camera(hal.AbstractCamera):
         #   Proper adherence to the HTTP protocol must be used when writing to this stream in order to achieve successful interoperation with HTTP clients.
         #   Changed in version 3.6: This is an io.BufferedIOBase stream.
         # data = imageio.imread(BytesIO(requests.get(self.url+"scan.jpg").content))
-        _ = requests.get(self.url + "/scan.jpg")  # update the picture
+        # _ = requests.get(self.url + "/scan.jpg")  # update the picture
         data = imageio.imread(self.url + "/scan.jpg")  # download it
         data_item.add_channel(self.channels()[0], data)
         return data_item
