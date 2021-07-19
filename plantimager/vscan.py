14d803c514bab47c03bb3df919ca9a8a8343ab98
Nabil AIT TALEB <aittalebnabil@gmail.com>
2021-07-19 15:06:16 +0200

    Fix id mismatch in metadata

--- plantimager/vscan.py
+++ plantimager/vscan.py
@@ -183,13 +183,18 @@ class VirtualScanner(AbstractScanner):
             files = { "file" : open(file_path, "rb")}
             return self.request_post("upload_background", {}, files)
 
-    def scan(self, path: path.Path, fileset: Fileset, channels) -> None:
+    def scan(self, path: path.Path, rgb_channel_fileset: Fileset, other_channels_fileset: Fileset) -> None:
         for x in path:
             pose = self.get_target_pose(x)
             print(pose)
             data_item = self.scan_at(pose, x.exact_pose)
-            for c in channels:
-                f = fileset.create_file(data_item.channels[c].format_id())
+            for c in self.channels():
+                if c == 'rgb':
+                    f = rgb_channel_fileset.create_file(data_item.channels[c].format_id())
+                else:
+                    if other_channels_fileset is not None:
+                       f = other_channels_fileset.create_file(data_item.channels[c].format_id())
+
                 io.write_image(f, data_item.channels[c].data, ext=self.ext)
                 if data_item.metadata is not None:
                     f.set_metadata(data_item.metadata)
