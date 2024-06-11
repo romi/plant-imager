532fee9903cf1f51552e6f89450b09a18e1f4fbf
Jonathan LEGRAND <jonathan.legrand@ens-lyon.fr>
2024-06-12 00:31:21 +0200

    Add missing callback

--- src/plantimager/webui/app.py
+++ src/plantimager/webui/app.py
@@ -167,6 +167,16 @@ def main(url, port):
         ]
     )
 
+    # we use a callback to toggle the collapse on small screens
+    @callback(
+        Output("navbar-collapse", "is_open"),
+        [Input("navbar-toggler", "n_clicks")],
+        [State("navbar-collapse", "is_open")])
+    def toggle_navbar_collapse(n, is_open):
+        if n:
+            return not is_open
+        return is_open
+
     return app
 
 
